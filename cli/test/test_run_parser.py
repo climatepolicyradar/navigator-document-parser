a885de2d6c06da3dcf0a2187c5b7863c5a23efc7
Kalyan Dutia <kalyan.dutia@gmail.com>
2022-09-08 17:50:12 +0100

    test for CLI

---
+++ cli/test/test_run_parser.py
@@ -0,0 +1,20 @@
+from pathlib import Path
+import tempfile
+
+import pytest
+from click.testing import CliRunner
+
+from cli.run_parser import main as cli_main
+
+
+@pytest.mark.filterwarnings("ignore::urllib3.exceptions.InsecureRequestWarning")
+def test_run_parser():
+    input_dir = str((Path(__file__).parent / "data" / "input").resolve())
+
+    with tempfile.TemporaryDirectory() as output_dir:
+        runner = CliRunner()
+        result = runner.invoke(cli_main, [input_dir, output_dir])
+
+        assert result.exit_code == 0
+
+        assert (Path(output_dir) / "test_id.json").exists()
