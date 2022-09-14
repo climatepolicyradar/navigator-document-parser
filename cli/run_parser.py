08a221c3eef9e4de7a2526363f2d6fee72dca027
Kalyan Dutia <kalyan.dutia@gmail.com>
2022-09-14 17:24:03 +0100

    skip files that have already been processed

--- cli/run_parser.py
+++ cli/run_parser.py
@@ -59,8 +59,15 @@ logging.config.dictConfig(DEFAULT_LOGGING)
 @click.option(
     "--files",
     "-f",
-    help="Pass in a list of filenames to parse, relative to the input directory.",
+    help="Pass in a list of filenames to parse, relative to the input directory. Used to optionally specify a subset of files to parse.",
     multiple=True,
+)
+@click.option(
+    "--redo",
+    "-r",
+    help="Redo parsing for files that have already been parsed. By default, files with IDs that already exist in the output directory are skipped.",
+    is_flag=True,
+    default=False,
 )
 def main(
     input_dir: Path,
@@ -68,13 +75,23 @@ def main(
     parallel: bool,
     device: str,
     files: Optional[List[str]],
+    redo: bool,
 ):
     """
     Run the parser on a directory of JSON files specifying documents to parse, and save the results to an output directory.
 
     :param input_dir: directory of input JSON files (task specifications)
     :param output_dir: directory of output JSON files (results)
+    :param parallel: whether to run PDF parsing over multiple processes
+    :param device: device to use for PDF parsing
+    :param files: list of filenames to parse, relative to the input directory. Can be used to select a subset of files to parse.
+    :param redo: redo parsing for files that have already been parsed. Defaults to False,
     """
+
+    # This relies on the names of output files being {id}.json at the moment, as otherwise we have to guess whether a file is serialised from PDF or JSON.
+    document_ids_previously_parsed = set(
+        [path.stem for path in output_dir.glob("*.json")]
+    )
 
     files_to_parse = (
         (input_dir / f for f in files) if files else input_dir.glob("*.json")
@@ -82,10 +99,20 @@ def main(
 
     tasks = [ParserInput.parse_file(_path) for _path in files_to_parse]
 
+    if not redo and document_ids_previously_parsed.intersection(
+        {task.id for task in tasks}
+    ):
+        logger.warning(
+            f"Found {len(document_ids_previously_parsed.intersection({task.id for task in tasks}))} documents that have already parsed. Skipping."
+        )
+        tasks = [
+            task for task in tasks if task.id not in document_ids_previously_parsed
+        ]
+
     html_tasks = [task for task in tasks if task.content_type == "text/html"]
     pdf_tasks = [task for task in tasks if task.content_type == "application/pdf"]
 
-    logger.info(f"Fount {len(html_tasks)} HTML tasks and {len(pdf_tasks)} PDF tasks")
+    logger.info(f"Found {len(html_tasks)} HTML tasks and {len(pdf_tasks)} PDF tasks")
 
     logger.info(f"Running HTML parser on {len(html_tasks)} documents")
     run_html_parser(html_tasks, output_dir)
--- cli/test/test_run_parser.py
+++ cli/test/test_run_parser.py
@@ -5,6 +5,7 @@ import pytest
 from click.testing import CliRunner
 
 from cli.run_parser import main as cli_main
+from src.base import HTMLParserOutput, PDFParserOutput
 
 
 @pytest.mark.filterwarnings("ignore::urllib3.exceptions.InsecureRequestWarning")
@@ -31,10 +32,64 @@ def test_run_parser_specific_files() -> None:
     with tempfile.TemporaryDirectory() as output_dir:
         runner = CliRunner()
         result = runner.invoke(
-            cli_main, [input_dir, output_dir, "--parallel", "--files", "test_pdf.json"]
+            cli_main, [input_dir, output_dir, "--parallel", "--files", "test_html.json"]
         )
 
         assert result.exit_code == 0
 
-        assert not (Path(output_dir) / "test_html.json").exists()
-        assert (Path(output_dir) / "test_pdf.json").exists()
+        assert (Path(output_dir) / "test_html.json").exists()
+        assert not (Path(output_dir) / "test_pdf.json").exists()
+
+
+@pytest.mark.filterwarnings("ignore::urllib3.exceptions.InsecureRequestWarning")
+def test_run_parser_skip_already_done(caplog) -> None:
+    """Test that files which have already been parsed are skipped by default."""
+
+    input_dir = str((Path(__file__).parent / "test_data" / "input").resolve())
+
+    with tempfile.TemporaryDirectory() as output_dir:
+        with open(Path(output_dir) / "test_pdf.json", "w") as f:
+            f.write(
+                PDFParserOutput.parse_obj(
+                    {
+                        "id": "test_pdf",
+                        "url": "https://www.pdfs.org",
+                        "languages": ["en"],
+                        "text_blocks": [],
+                        "document_slug": "slug",
+                        "page_metadata": [],
+                        "md5sum": "abcdefg",
+                    }
+                ).json()
+            )
+
+        with open(Path(output_dir) / "test_html.json", "w") as f:
+            f.write(
+                HTMLParserOutput.parse_obj(
+                    {
+                        "id": "test_html",
+                        "url": "https://www.google.org",
+                        "languages": ["en"],
+                        "text_blocks": [],
+                        "date": None,
+                        "has_valid_text": False,
+                        "document_slug": "slug",
+                    }
+                ).json()
+            )
+
+        runner = CliRunner()
+        result = runner.invoke(
+            cli_main,
+            [
+                input_dir,
+                output_dir,
+                "--parallel",
+            ],
+        )
+
+        assert result.exit_code == 0
+
+        assert (
+            "Found 2 documents that have already parsed. Skipping." in caplog.messages
+        )
