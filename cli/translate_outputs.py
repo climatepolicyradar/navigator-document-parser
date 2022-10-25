c1462e75ec73ada979b6abd01bf3e43661eb5427
Mark <mark@climatepolicyradar.org>
2022-10-25 09:31:12 +0100

    Integrating debug logs.

--- cli/translate_outputs.py
+++ cli/translate_outputs.py
@@ -20,11 +20,15 @@ def translate_parser_outputs(parser_output_dir: Union[Path, CloudPath]) -> None:
     """
 
     for path in tqdm(parser_output_dir.glob("*.json")):
+        logger.debug(f"Translating {path}.")
+
         parser_output = ParserOutput.parse_raw(path.read_text())
+        logger.debug(f"Successfuly parsed {path} for translation.")
 
         # Skip already translated outputs. Note this does not prevent the CLI from translating existing parser outputs again,
         # but instead makes sure it doesn't translate a translation.
         if parser_output.translated:
+            logger.debug(f"parser.translated true for - {path}.")
             continue
 
         _target_languages = set(TARGET_LANGUAGES)
@@ -34,7 +38,10 @@ def translate_parser_outputs(parser_output_dir: Union[Path, CloudPath]) -> None:
         if parser_output.languages and len(parser_output.languages) == 1:
             _target_languages = _target_languages - set(parser_output.languages)
 
+        logger.debug(f"Target languages for {path}: {_target_languages}.")
+
         for target_language in _target_languages:
+            logger.debug(f"Translating {path} to {target_language}.")
             output_path = path.with_name(
                 f"{path.stem}_translated_{target_language}.json"
             )
@@ -47,7 +54,9 @@ def translate_parser_outputs(parser_output_dir: Union[Path, CloudPath]) -> None:
             translated_parser_output = translate_parser_output(
                 parser_output, target_language
             )
+            logger.debug(f"Translated {path} to {target_language}.")
 
             output_path.write_text(
                 translated_parser_output.json(indent=4, ensure_ascii=False)
             )
+            logger.debug(f"Saved translated output to {output_path}.")
