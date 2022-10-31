ce65ec8aa48f4f33a2de266477e93645f7b4b57b
Mark <mark@climatepolicyradar.org>
2022-10-31 15:04:53 +0000

    Update the translation stage of the parser to use functions with unit tests and refactor.

--- cli/test/test_run_parser.py
+++ cli/test/test_run_parser.py
@@ -1,15 +1,17 @@
 from pathlib import Path
 import tempfile
 from unittest import mock
-
+from typing import Sequence, Union
 import pytest
 from click.testing import CliRunner
 
 from cloudpathlib.local import LocalS3Path
 
 from cli.run_parser import main as cli_main
+from cli.translate_outputs import should_be_translated, identify_translation_languages
 
-from src.base import ParserOutput
+from src.base import ParserOutput, HTMLData
+from src.config import TARGET_LANGUAGES
 
 patcher = mock.patch(
     "src.translator.translate.translate_text",
@@ -153,5 +155,58 @@ def test_run_parser_skip_already_done(caplog) -> None:
         assert result.exit_code == 0
 
         assert (
-            "Skipping 2 documents that have already been parsed." in caplog.messages
+                "Skipping 2 documents that have already been parsed." in caplog.messages
         )
+
+
+_target_languages = set(TARGET_LANGUAGES)
+
+
+def get_parser_output(translated: bool, source_url: Union[str, None], languages: Sequence[str]) -> ParserOutput:
+    """Generate the parser output objects for the tests given input variables."""
+    return ParserOutput(
+        document_id='sdf',
+        document_metadata={},
+        document_name='sdf',
+        document_description='sdf',
+        document_source_url=source_url,
+        document_cdn_object='sdf',
+        document_content_type="text/html",
+        document_md5_sum='sdf',
+        document_slug='sdf',
+        languages=languages,
+        translated=translated,
+        html_data=HTMLData(
+            text_blocks=[],
+            detected_date=None,
+            detected_title="",
+            has_valid_text=False,
+        ),
+        pdf_data=None
+    )
+
+
+@pytest.mark.filterwarnings("ignore::urllib3.exceptions.InsecureRequestWarning")
+def test_should_be_translated() -> None:
+    """Tests that the output from the function is correct for known input documents."""
+    doc_1 = get_parser_output(translated=False, source_url="https://www.google.org", languages=['fr'])
+    assert should_be_translated(doc_1) is True
+
+    doc_2 = get_parser_output(translated=False, source_url=None, languages=['fr'])
+    assert should_be_translated(doc_2) is False
+
+    doc_3 = get_parser_output(translated=False, source_url="https://www.google.org", languages=['English'])
+    assert should_be_translated(doc_3) is True
+
+    doc_4 = get_parser_output(translated=True, source_url="https://www.google.org", languages=['fr'])
+    assert should_be_translated(doc_4) is False
+
+
+@pytest.mark.filterwarnings("ignore::urllib3.exceptions.InsecureRequestWarning")
+def test_identify_target_languages() -> None:
+    """Tests that the output from the function is correct for known input documents."""
+    doc_1 = get_parser_output(translated=False, source_url="https://www.google.org", languages=['fr'])
+    assert identify_translation_languages(doc_1, _target_languages) == {'en'}
+
+    doc_2 = get_parser_output(translated=False, source_url="https://www.google.org", languages=['en'])
+    assert identify_translation_languages(doc_2, _target_languages) == set()
