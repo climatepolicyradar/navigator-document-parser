c56b721bc01e9f91a5dd67a01d95fca1bbe9329a
Joel Wright <joel@climatepolicyradar.org>
2022-11-03 17:34:30 +0000

    Do not bail out of document parsing due to raised exception (#44)
    * Don't bail out on parse pdf exception
    
    * Don't bail out on HTML parse exception
    
    * Don't bail out on translation due to a raised exception
    
    * Use logger.exception, not logger.error

--- cli/translate_outputs.py
+++ cli/translate_outputs.py
@@ -13,9 +13,9 @@ from src.base import ParserOutput
 from src.config import TARGET_LANGUAGES, LOGGING_LEVEL
 from src.translator.translate import translate_parser_output
 
-logger = logging.getLogger(__name__)
+_LOGGER = logging.getLogger(__name__)
 level = logging.getLevelName(LOGGING_LEVEL)
-logger.setLevel(level)
+_LOGGER.setLevel(level)
 
 
 def should_be_translated(document: ParserOutput) -> bool:
@@ -39,7 +39,7 @@ def identify_translation_languages(document: ParserOutput, target_languages: Set
     # TODO: how do we deal with the fact that parser outputs can contain multiple languages here?
     if document.languages and len(document.languages) == 1:
         minus = set(document.languages)
-        logger.debug(f"Removing {minus if minus is not None else None} from {target_languages}.")
+        _LOGGER.debug(f"Removing {minus if minus is not None else None} from {target_languages}.")
         target_languages = target_languages - set(document.languages)
     return target_languages
 
@@ -53,24 +53,24 @@ def translate_parser_outputs(task_output_paths: Sequence[Union[Path, CloudPath]]
     _target_languages = set(TARGET_LANGUAGES)
 
     for path in tqdm(task_output_paths):
-        logger.debug(f"Translator processing - {path}.")
+        _LOGGER.debug(f"Translator processing - {path}.")
 
         try:
             parser_output = ParserOutput.parse_raw(path.read_text())
-            logger.debug(f"Successfully parsed {path} from output dir during translation processing.")
+            _LOGGER.debug(f"Successfully parsed {path} from output dir during translation processing.")
         except FileNotFoundError:
-            logger.error(f"Could not find {path} in output dir during translation processing.")
+            _LOGGER.error(f"Could not find {path} in output dir during translation processing.")
             continue
 
         if should_be_translated(parser_output):
-            logger.info(f"Document should be translated: {path}")
+            _LOGGER.info(f"Document should be translated: {path}")
 
             target_languages = identify_translation_languages(parser_output, _target_languages)
-            logger.debug(f"Target languages: {target_languages} for {path}")
+            _LOGGER.debug(f"Target languages: {target_languages} for {path}")
 
             _translate_to_target_languages(path, parser_output, target_languages, redo=redo)
 
-    logger.info('Finished translation stage for ALL input tasks.')
+    _LOGGER.info('Finished translation stage for ALL input tasks.')
 
 
 def _translate_to_target_languages(
@@ -80,27 +80,33 @@ def _translate_to_target_languages(
     redo: bool = False
 ) -> None:
     for target_language in target_languages:
-        logger.info(f"Translating {path} to {target_language}.")
-
-        output_path = path.with_name(f"{path.stem}_translated_{target_language}.json")
-        if output_path.exists() and not redo:  # type: ignore
-            logger.info(f"Skipping translating {output_path} because it already exists.")
-            continue
-
-        translated_parser_output = translate_parser_output(
-            parser_output, target_language
-        )
-        logger.info(f"Translated {path} to {target_language}.")
-
         try:
-            output_path.write_text(  # type: ignore
-                        translated_parser_output.json(indent=4, ensure_ascii=False)
-                    )
-            logger.info(f"Saved translated output to {output_path}.")
+            _LOGGER.info(f"Translating {path} to {target_language}.")
 
-        except cloudpathlib.exceptions.OverwriteNewerCloudError:
-            logger.info(
-                f"Tried to write to {output_path}, received OverwriteNewerCloudError, "
-                "assuming a newer task definition is the one we want, continuing to "
-                "process."
+            output_path = path.with_name(f"{path.stem}_translated_{target_language}.json")
+            if output_path.exists() and not redo:  # type: ignore
+                _LOGGER.info(f"Skipping translating {output_path} because it already exists.")
+                continue
+
+            translated_parser_output = translate_parser_output(
+                parser_output, target_language
+            )
+            _LOGGER.info(f"Translated {path} to {target_language}.")
+
+            try:
+                output_path.write_text(  # type: ignore
+                            translated_parser_output.json(indent=4, ensure_ascii=False)
+                        )
+                _LOGGER.info(f"Saved translated output to {output_path}.")
+
+            except cloudpathlib.exceptions.OverwriteNewerCloudError:
+                _LOGGER.info(
+                    f"Tried to write to {output_path}, received OverwriteNewerCloudError, "
+                    "assuming a newer task definition is the one we want, continuing to "
+                    "process."
+                )
+        except Exception:
+            _LOGGER.exception(
+                "Failed to successfully translate due to a raised exception",
+                extra={"props": {"input_path": f"{path}", "target_language": target_language}},
             )
