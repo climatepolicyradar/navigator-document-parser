170eac4c14444548f378fd9d75d9fd874ae0dee6
Mark <mark@climatepolicyradar.org>
2023-05-23 09:47:28 +0100

    Updating doc string.

--- cli/warm_up_model.py
+++ cli/warm_up_model.py
@@ -1,13 +1,16 @@
-"""This module presents a method for downloading the model that is to be used in the parser so that it can be
-downloaded once and form a layer of the docker image rather than being downloaded at run time every time the
-parser is instantiated. """
+"""Module presents a method for downloading the model that is to be used in the parser.
+
+This is so that it can be downloaded once and form a layer of the docker image rather than being downloaded at
+run time every time the parser is instantiated.
+"""
+
 import os
 
 # Set cdn domain as the environment variable is required by the run_parser.py script
 os.environ["CDN_DOMAIN"] = "cdn.dummy.com"
 
-from cli.parse_pdfs import get_model
-from src.config import LAYOUTPARSER_MODEL, PDF_OCR_AGENT
+from cli.parse_pdfs import get_model  # noqa: E402
+from src.config import LAYOUTPARSER_MODEL, PDF_OCR_AGENT  # noqa: E402
 
 
 if __name__ == "__main__":
