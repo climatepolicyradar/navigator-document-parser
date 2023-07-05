e2ca20a80608c285bed37274c2adde3a9bb57492
Joel Wright <joel@climatepolicyradar.org>
2023-07-05 18:00:38 +0100

    Updates to base image & dependencies to fix build issues

--- cli/run.sh
+++ cli/run.sh
@@ -4,4 +4,4 @@ set -e
 mkdir /app/credentials/
 echo "${GOOGLE_CREDS}" | base64 -d > /app/credentials/google-creds.json
 export GOOGLE_APPLICATION_CREDENTIALS=/app/credentials/google-creds.json
-python -m cli.run_parser --s3 "${PARSER_INPUT_PREFIX}" "${EMBEDDINGS_INPUT_PREFIX}"
+python3 -m cli.run_parser --s3 "${PARSER_INPUT_PREFIX}" "${EMBEDDINGS_INPUT_PREFIX}"
