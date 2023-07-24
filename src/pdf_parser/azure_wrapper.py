3f5a2214c87df59393b1fff4ac94bc2e1b531155
Mark <mark@climatepolicyradar.org>
2023-07-24 14:35:28 +0200

    Adding in the refactors.

--- src/pdf_parser/azure_wrapper.py
+++ src/pdf_parser/azure_wrapper.py
@@ -17,28 +17,33 @@ logger = logging.getLogger(__name__)
 def poller_loop(
     poller: Union[
         DocumentAnalysisClient.begin_analyze_document_from_url,
-        DocumentAnalysisClient.begin_analyze_document
+        DocumentAnalysisClient.begin_analyze_document,
     ]
 ) -> None:
     """Poll the status of the poller until it is done."""
     counter = 0
-    logger.info(f'Poller status {poller.status()}...')
+    logger.info(f"Poller status {poller.status()}...")
     while not poller.done():
         time.sleep(0.2)
         counter += 1
         if counter % 50 == 0:
-            logger.info(f'Poller status {poller.status()}...')
-    logger.info(f'Poller status {poller.status()}...')
+            logger.info(f"Poller status {poller.status()}...")
+    logger.info(f"Poller status {poller.status()}...")
 
 
 def call_api_with_error_handling(retries: int, func, *args, **kwargs) -> None:
     """Call an API function with retries and error handling."""
-    logger.info(f'Calling API function with retries...', extra={"props": {"retries": retries}})
+    logger.info(
+        "Calling API function with retries...", extra={"props": {"retries": retries}}
+    )
     for i in range(retries):
         try:
             return func(*args, **kwargs)
         except Exception as e:
-            logger.error(f'Error occurred while calling API function...', extra={"props": {"error": str(e)}})
+            logger.error(
+                "Error occurred while calling API function...",
+                extra={"props": {"error": str(e)}},
+            )
             if i == retries - 1:
                 raise e
 
@@ -47,27 +52,40 @@ class AzureApiWrapper:
     """Wrapper for Azure Form Extraction API."""
 
     def __init__(self, key: str, endpoint: str):
-        logger.info(f'Initializing Azure API wrapper with endpoint...', extra={"props": {"endpoint": endpoint}})
+        logger.info(
+            "Initializing Azure API wrapper with endpoint...",
+            extra={"props": {"endpoint": endpoint}},
+        )
         self.document_analysis_client = DocumentAnalysisClient(
-            endpoint=endpoint, credential=AzureKeyCredential(key),
+            endpoint=endpoint,
+            credential=AzureKeyCredential(key),
         )
 
-    def analyze_document_from_url(self, doc_url: str, timeout: Optional[Union[int, None]] = None) -> AnalyzeResult:
+    def analyze_document_from_url(
+        self, doc_url: str, timeout: Optional[Union[int, None]] = None
+    ) -> AnalyzeResult:
         """Analyze a pdf document accessible by an endpoint."""
-        logger.info('Analyzing document from url...', extra={"props": {"url": doc_url}})
+        logger.info("Analyzing document from url...", extra={"props": {"url": doc_url}})
         poller = self.document_analysis_client.begin_analyze_document_from_url(
-            'prebuilt-document', doc_url,
+            "prebuilt-document",
+            doc_url,
         )
 
         poller_loop(poller)
 
         return poller.result(timeout=timeout)
 
-    def analyze_document_from_bytes(self, doc_bytes: bytes, timeout: Optional[Union[int, None]] = None) -> AnalyzeResult:
+    def analyze_document_from_bytes(
+        self, doc_bytes: bytes, timeout: Optional[Union[int, None]] = None
+    ) -> AnalyzeResult:
         """Analyze a pdf document in the form of bytes."""
-        logger.info('Analyzing document from bytes...', extra={"props": {"bytes_size": sys.getsizeof(doc_bytes)}})
+        logger.info(
+            "Analyzing document from bytes...",
+            extra={"props": {"bytes_size": sys.getsizeof(doc_bytes)}},
+        )
         poller = self.document_analysis_client.begin_analyze_document(
-            'prebuilt-document', doc_bytes,
+            "prebuilt-document",
+            doc_bytes,
         )
 
         poller_loop(poller)
@@ -75,18 +93,19 @@ class AzureApiWrapper:
         return poller.result(timeout=timeout)
 
     def analyze_large_document_from_url_split(
-            self, doc_url: str, timeout: Optional[Union[int, None]] = None) -> Tuple[Sequence[PDFPage], AnalyzeResult]:
+        self, doc_url: str, timeout: Optional[Union[int, None]] = None
+    ) -> Tuple[Sequence[PDFPage], AnalyzeResult]:
         """Analyze a large pdf document accessible by an endpoint by splitting into individual pages."""
         logger.info(
-            'Analyzing large document from url by splitting into individual pages...',
-            extra={"props": {"url": doc_url}}
+            "Analyzing large document from url by splitting into individual pages...",
+            extra={"props": {"url": doc_url}},
         )
         try:
             resp = requests.get(doc_url)
             pdf_bytes = BytesIO(resp.content)
 
             pages_dict = split_into_pages(document_bytes=pdf_bytes)
-            logger.info(f'Number of pages: {len(pages_dict)}')
+            logger.info(f"Number of pages: {len(pages_dict)}")
             page_api_responses = [
                 PDFPage(
                     page_number=page_num,
@@ -104,8 +123,8 @@ class AzureApiWrapper:
 
         except Exception as e:
             logger.error(
-                'Error occurred while analyzing large document from url by splitting into individual pages...',
-                extra={"props": {"url": doc_url}}
+                "Error occurred while analyzing large document from url by splitting into individual pages...",
+                extra={"props": {"url": doc_url}},
             )
             raise e
 
@@ -113,12 +132,12 @@ class AzureApiWrapper:
             self, doc_bytes: bytes, timeout: Optional[Union[int, None]] = None) -> Tuple[Sequence[PDFPage], AnalyzeResult]:
         """Analyze a large pdf document in the bytes form by splitting into individual pages."""
         logger.info(
-            'Analyzing large document from bytes by splitting into individual pages...',
+            "Analyzing large document from bytes by splitting into individual pages...",
             extra={"props": {"bytes_size": sys.getsizeof(doc_bytes)}}
         )
         try:
             pages_dict = split_into_pages(document_bytes=io.BytesIO(doc_bytes))
-            logger.info(f'Number of pages: {len(pages_dict)}')
+            logger.info(f"Number of pages: {len(pages_dict)}")
             page_api_responses = [
                 PDFPage(
                     page_number=page_num,
@@ -136,7 +155,7 @@ class AzureApiWrapper:
 
         except Exception as e:
             logger.error(
-                'Error occurred while analyzing large document from bytes by splitting into individual pages...',
+                "Error occurred while analyzing large document from bytes by splitting into individual pages...",
                 extra={"props": {"bytes_size": sys.getsizeof(doc_bytes)}}
             )
             raise e
--- src/pdf_parser/utils.py
+++ src/pdf_parser/utils.py
@@ -11,84 +11,97 @@ from src.base import ParserOutput, PDFTextBlock, PDFData, \
 
 def polygon_to_coords(polygon: Sequence[Point]) -> list[tuple[float, float]]:
     """Converts a polygon (four x,y co-ordinates) to a list of co-ordinates (two x,y points).
-    The co-ordinates represent the box as (x1, y1), (x2, y2).
+
+    The origin of the co-ordinate system is the top left corner of the page.
 
     The points array is ordered as follows:
-    - bottom left, bottom right, top right, top left
+    - top left, top  right, bottom right, bottom left
     """
-    # FIXME: Check that these are the co-ordinates we want to form the box
-    #  (are we using bottom right, top left elsewhere?)
-    return [(polygon[1].x, polygon[1].y), (polygon[3].x, polygon[3].y)]
+
+    if len(polygon) != 4:
+        raise ValueError("Polygon must have exactly four points.")
+
+    return [(vertex.x, vertex.y) for vertex in polygon]
 
 
-def convert_to_text_block(paragraph_id: int, paragraph: DocumentParagraph) -> PDFTextBlock:
+def convert_to_text_block(
+    paragraph_id: int, paragraph: DocumentParagraph
+) -> PDFTextBlock:
     """Convert a DocumentParagraph to a PDFTextBlock."""
     return PDFTextBlock(
         coords=polygon_to_coords(paragraph.bounding_regions[0].polygon),
         # FIXME: The paragraph could be split across multiple pages, page_number only allows int
         page_number=paragraph.bounding_regions[0].page_number,
         text=[paragraph.content],
-        text_block_id=paragraph_id,
+        text_block_id=str(paragraph_id),
         language=None,
-        type=paragraph.role if paragraph.role is not None else 'Ambiguous',
-        type_confidence=0.0,
+        type=paragraph.role or "Ambiguous",
+        type_confidence=1.0,
     )
 
 
 def convert_to_parser_output(parser_input: ParserInput, md5sum: str, api_response: AnalyzeResult) -> ParserOutput:
     """Convert the API response AnalyzeResult object to a ParserOutput."""
-    return ParserOutput(
-        document_id=parser_input.document_id,
-        document_metadata=parser_input.document_metadata,
-        document_name=parser_input.document_name,
-        document_description=parser_input.document_description,
-        document_source_url=parser_input.document_source_url,
-        document_cdn_object=parser_input.document_cdn_object,
-        document_content_type=parser_input.document_cdn_object,
-        document_md5_sum=md5sum,
-        document_slug=parser_input.document_slug,
-        languages=None,
-        translated=False,
-        html_data=None,
-        pdf_data=PDFData(
-            # FIXME: Check that the units of the dimensions are correct (units are in inches)
-            page_metadata=[
-                PDFPageMetadata(
-                    page_number=page.page_number,
-                    dimensions=(page.width, page.height),
-                )
-                for page in api_response.pages
-            ],
-            md5sum=md5sum,
-            text_blocks=[
-                convert_to_text_block(paragraph_id=index, paragraph=paragraph)
-                for index, paragraph in enumerate(api_response.paragraphs)
-            ],
-            table_blocks=[
-                PDFTableBlock(
-                    table_id=0,
-                    row_count=table.row_count,
-                    column_count=table.column_count,
-                    cells=[
-                        TableCell(
-                            cell_type=cell.kind,
-                            row_index=cell.row_index,
-                            column_index=cell.column_index,
-                            row_span=cell.row_span,
-                            column_span=cell.column_span,
-                            content=cell.content,
-                            bounding_regions=[
-                                BoundingRegion(
-                                    page_number=cell.bounding_regions[0].page_number,
-                                    polygon=cell.bounding_regions[0].polygon
-                                )
-                            ]
-                        )
-                        for cell in table.cells
-                    ]
-                ) for index, table in enumerate(api_response.tables)],
-        ),
-    ).detect_and_set_languages().set_document_languages_from_text_blocks()
+    return (
+        ParserOutput(
+            document_id=parser_input.document_id,
+            document_metadata=parser_input.document_metadata,
+            document_name=parser_input.document_name,
+            document_description=parser_input.document_description,
+            document_source_url=parser_input.document_source_url,
+            document_cdn_object=parser_input.document_cdn_object,
+            document_content_type=parser_input.document_cdn_object,
+            document_md5_sum=md5sum,
+            document_slug=parser_input.document_slug,
+            languages=None,
+            translated=False,
+            html_data=None,
+            pdf_data=PDFData(
+                # FIXME: Check that the units of the dimensions are correct (units are in inches)
+                page_metadata=[
+                    PDFPageMetadata(
+                        page_number=page.page_number,
+                        dimensions=(page.width, page.height),
+                    )
+                    for page in api_response.pages
+                ],
+                md5sum=md5sum,
+                text_blocks=[
+                    convert_to_text_block(paragraph_id=index, paragraph=paragraph)
+                    for index, paragraph in enumerate(api_response.paragraphs)
+                ],
+                table_blocks=[
+                    PDFTableBlock(
+                        table_id=0,
+                        row_count=table.row_count,
+                        column_count=table.column_count,
+                        cells=[
+                            TableCell(
+                                cell_type=cell.kind,
+                                row_index=cell.row_index,
+                                column_index=cell.column_index,
+                                row_span=cell.row_span,
+                                column_span=cell.column_span,
+                                content=cell.content,
+                                bounding_regions=[
+                                    BoundingRegion(
+                                        page_number=cell.bounding_regions[
+                                            0
+                                        ].page_number,
+                                        polygon=cell.bounding_regions[0].polygon,
+                                    )
+                                ],
+                            )
+                            for cell in table.cells
+                        ],
+                    )
+                    for index, table in enumerate(api_response.tables)
+                ],
+            ),
+        )
+        .detect_and_set_languages()
+        .set_document_languages_from_text_blocks()
+    )
 
 
 def propagate_with_correct_page_number(page: PDFPage) -> PDFPage:
@@ -121,10 +134,7 @@ def merge_responses(pages: Sequence[PDFPage]) -> AnalyzeResult:
         for page in pages[1:]
     ]
 
-    [
-        page_merged.tables.append(page.extracted_content.tables)
-        for page in pages[1:]
-    ]
+    [page_merged.tables.append(page.extracted_content.tables) for page in pages[1:]]
 
     return page_merged
 
