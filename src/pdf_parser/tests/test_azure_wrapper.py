77f2c5d86836eb886df3350b5128fc2b9b45eaf6
Mark <mark@climatepolicyradar.org>
2023-07-25 15:52:02 +0200

    Further refactoring.

--- src/pdf_parser/tests/test_azure_wrapper.py
+++ src/pdf_parser/tests/test_azure_wrapper.py
@@ -1,41 +1,39 @@
+import unittest
 from unittest.mock import patch
 
 from azure.ai.formrecognizer import AnalyzeResult
 
 from src.base import PDFPage
-from src.pdf_parser.azure_wrapper import call_api_with_error_handling
-
-
-# TODO Test that call api with errors retries the set amount of times
-#   Test that the poller continues until done
+from src.pdf_parser.azure_wrapper import call_api_with_error_handling, AzureApiWrapper
 
 
 def test_call_api_with_error_handling_good_response(
-    mock_azure_client,
-    one_page_pdf_bytes
-):
+    mock_azure_client: AzureApiWrapper,
+    one_page_pdf_bytes: bytes,
+    one_page_mock_analyse_result: AnalyzeResult
+) -> None:
     """Test the retry logic and exception handling to the function when the api
-    response is good. """
+    response is good."""
 
-    call_api_with_error_handling(
+    response = call_api_with_error_handling(
         retries=3,
         func=mock_azure_client.analyze_document_from_url,
         doc_bytes=one_page_pdf_bytes,
         timeout=None
     )
 
+    assert response == one_page_mock_analyse_result
     assert mock_azure_client.analyze_document_from_url.call_count is 1
 
 
 def test_call_api_with_error_handling_bad_response(
-        mock_azure_client,
-        one_page_pdf_bytes
-):
+    mock_azure_client: AzureApiWrapper,
+    one_page_pdf_bytes: bytes,
+) -> None:
     """Test the retry logic and exception handling to the function when the api
-    response is bad. """
+    response is bad."""
     retries = 3
     exception_to_raise = Exception("Simulated API error")
-
     mock_azure_client.analyze_document_from_url.side_effect = exception_to_raise
 
     exception_raised = None
@@ -51,38 +49,49 @@ def test_call_api_with_error_handling_bad_response(
 
     assert exception_raised == exception_to_raise
     assert mock_azure_client.analyze_document_from_url.call_count is retries
-    assert mock_azure_client.analyze_document_from_url
 
 
-def test_analyze_document_from_url(mock_azure_client, one_page_mock_analyse_result):
+def test_analyze_document_from_url(
+    mock_azure_client: AzureApiWrapper,
+    one_page_mock_analyse_result: AnalyzeResult
+) -> None:
     """Test that the document from url method returns the correct response."""
     response = mock_azure_client.analyze_document_from_url(
-        "https://example.com/test.pdf")
+        "https://example.com/test.pdf"
+    )
 
     assert mock_azure_client.analyze_document_from_url.call_count is 1
     assert response == one_page_mock_analyse_result
 
 
-def test_analyze_document_from_bytes(mock_azure_client, one_page_mock_analyse_result):
+def test_analyze_document_from_bytes(
+    mock_azure_client: AzureApiWrapper,
+    one_page_mock_analyse_result: AnalyzeResult
+) -> None:
     """Test that the document from bytes method returns the correct response."""
     response = mock_azure_client.analyze_document_from_bytes(
-        bytes("Random Content".encode("UTF-8")))
+        bytes("Random Content".encode("UTF-8"))
+    )
 
     assert response == one_page_mock_analyse_result
 
 
-def test_document_split_one_page(mock_azure_client, one_page_mock_analyse_result,
-                                 mock_document_download_response_one_page):
+def test_document_split_one_page(
+    mock_azure_client: AzureApiWrapper,
+    one_page_mock_analyse_result: AnalyzeResult,
+    mock_document_download_response_one_page: unittest.mock.Mock
+) -> None:
     """Test that processing a document via url with the multi page function returns
-    the correct response. """
-    # TODO can this go in a pytest fixture
+    the correct response."""
     with patch('requests.get') as mock_get:
         mock_get.return_value = mock_document_download_response_one_page
 
-        page_api_responses, merged_page_api_responses = \
-            mock_azure_client.analyze_large_document_from_url_split(
-                "https://example.com/test.pdf"
-            )
+        response = mock_azure_client.analyze_large_document_from_url_split(
+            "https://example.com/test.pdf"
+        )
+
+        page_api_responses = response[0]
+        merged_page_api_responses = response[1]
 
         assert isinstance(page_api_responses, list)
         assert len(page_api_responses) is 1
@@ -94,10 +103,10 @@ def test_document_split_one_page(mock_azure_client, one_page_mock_analyse_result
 
 
 def test_document_split_two_page(
-    mock_azure_client,
-    one_page_mock_analyse_result,
-    mock_document_download_response_two_page
-):
+    mock_azure_client: AzureApiWrapper,
+    one_page_mock_analyse_result: AnalyzeResult,
+    mock_document_download_response_two_page: unittest.mock.Mock
+) -> None:
     """Test that processing a document via url with the split page functionality
     returns the correct response.
 
